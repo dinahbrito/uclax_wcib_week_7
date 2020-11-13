6277df38873034002110c0c722fb94daacacaf54
dinahbrito <dinahbrito@gmail.com>
2020-11-12 16:56:02 -0800

    js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
 jQuery(function() {
 
+let my_switch = false;
+
+$('#my_button').click(function () {
+
+	if ( my_switch == false ) {
+		my_switch = true;
+		$('#my_image').attr('src', 'images/boy.jpg');
+	} else {
+		my_switch = false;
+		$('#my_image').attr('src', 'images/girl.jpg');
+	}
+
+});
 
 });
